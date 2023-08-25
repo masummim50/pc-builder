89588214e99842caa670e2c6b7d893a462330d56
unknown <masummim50@gmail.com>
2023-08-25 15:23:40 +0600

    featured components and categories

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,22 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import {AiFillStar} from "react-icons/ai"
 
 const ProductCard = ({ product }) => {
     return (
-        <Link href={`/product/${product._id}`} className='h-full rounded-lg border-t-4 border-t-pink-500/25 hover:border-t-pink-500'>
+        <Link href={`/product/${product._id}`} className='h-full rounded-lg border-t-4 border-t-indigo-500/25 hover:border-t-indigo-500'>
             <div className="p-3 shadow gap-3 rounded-lg hover:shadow-lg h-auto transition-all ease-in">
 
-            <div style={{ width: '100%', height: '100%', position: 'relative' }} className=' flex items-center justify-center'>
-                <Image
-                    alt='Mountains'
-                    src={product.image}
-                    width={200}
-                    height={200}
+                <div style={{ width: '100%', height: '100%', position: 'relative' }} className=' flex items-center justify-center'>
+                    <Image
+                        alt='Mountains'
+                        src={product.image}
+                        width={200}
+                        height={200}
                     />
+                </div>
+                <h2 className='font-bold h-[50px] text-gray-700 overflow-hidden mb-2'>{product.name}</h2>
+                <div className="flex justify-between">
+                    <div className="bg-pink-200 rounded-lg px-3 py-2 text-[11px]">{product.brand}</div>
+                    <div className="bg-purple-200 rounded-lg px-3 py-2 text-[11px]">{product.status}</div>
+                </div>
+                <div className='mt-2'><span className='font-bold'>Price: </span>{product.price.toLocaleString("en-US")} Bdt</div>
+                <div>
+                    {product.reviews.length}
+                    (
+                        {
+                            Array(product.rating).fill("").map((_,index)=> <AiFillStar className='inline text-orange-300' key={index}/>)
+                        }
+                    )
+                </div>
             </div>
-            <h2 className='font-bold h-[40px]'>{product.name}</h2>
-                    </div>
         </Link>
     );
 };
--- src/components/RandomProducts.js
+++ src/components/RandomProducts.js
@@ -3,13 +3,18 @@ import ProductCard from './ProductCard';
 
 const RandomProducts = ({products}) => {
     return (
+        <div className='mt-6 mb-3'>
+        <h2 className="text-gray-700 font-bold underline text-[30px] mb-3">
+            Featured Products
+        </h2>
         <div className='grid grid-cols-3 gap-4 container mx-auto'>
             {
                 products.map((product)=> (
                     <ProductCard className="col-span-1 bg-gray-200 rounded-lg h-full " product={product} key={product._id}/>
-                ))
-            }
+                    ))
+                }
         </div>
+                </div>
     );
 };
 
