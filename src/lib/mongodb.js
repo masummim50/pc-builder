52871bea324dece624bc5e5ba63b4c0e590d8da6
unknown <masummim50@gmail.com>
2023-08-26 11:22:31 +0600

    d-uri

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,7 +3,6 @@ const { default: mongoose } = require("mongoose")
 const connectToMongodb = async ()=> {
     try {
         await mongoose.connect(process.env.DATABASE_URI)
-        // await mongoose.connect("mongodb://127.0.0.1:27017/pcbuilder")
         console.log("database connected");
     } catch (error) {
         console.log("error connecting to database: ", error)
