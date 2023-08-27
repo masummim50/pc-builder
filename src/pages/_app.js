8fa4b5c13751d6c3cdb4d606156a153b60b0a2a7
unknown <masummim50@gmail.com>
2023-08-27 13:01:04 +0600

    sessions

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react"
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
   //   return getLayout(<SessionProvider session={session}>
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
 
   return(
   <Provider store={store}>
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       {
         getLayout(<Component {...pageProps} />)
       }
